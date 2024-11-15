d871870413c696383bc7bfaa87024c8ac9f0c493
Max Khrapatiy <max.khrapatiy@gmail.com>
2024-11-15 22:26:10 +0300

    Add swagger configuration.

---
+++ src/main/java/dev/khrapatiy/taskmanagementsystem/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package dev.khrapatiy.taskmanagementsystem.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Management System",
+                description = "Api for Task Management System",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Max Khrapatiy",
+                        email = "max.khrapatiy@gmail.com",
+                        url = "https://khrapatiy.dev"
+                )
+        ),
+        servers = @Server(url = "http://localhost:8080")
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
