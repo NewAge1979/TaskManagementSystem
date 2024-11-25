cc87ff89221c3d890c1b6b8c84aaf11941fb9e4c
Max Khrapatiy <max.khrapatiy@gmail.com>
2024-11-25 09:23:16 +0300

    TMS: Edit swagger configuration.

--- src/main/java/dev/khrapatiy/taskmanagementsystem/config/WebSecurityConfig.java
+++ src/main/java/dev/khrapatiy/taskmanagementsystem/config/WebSecurityConfig.java
@@ -32,7 +32,10 @@ public class WebSecurityConfig {
             "/swagger-ui/**",
             "/swagger-resource/*",
             "/v3/api-docs/**",
-            "/actuator/**"
+            "/actuator/**",
+            "/tmsapis/**",
+            "/tmsdocs/**",
+            "/tmsdocs.yaml"
     };
 
     private final UserDetailsService userDetailsService;
